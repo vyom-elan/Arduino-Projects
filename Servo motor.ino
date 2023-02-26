5b65df850be4c39d5a2db4b462874a78a41d6c2b
Vyom <97853828+vyom-elan@users.noreply.github.com>
2023-02-26 23:45:16 +0530

    Servo motor

---
+++ Servo motor.ino
@@ -0,0 +1,19 @@
+#include <Servo.h>
+Servo motor;
+int p=0;
+void setup() {
+  motor.attach(9);
+  motor.write(180);
+}
+void loop() {
+  for(p=0;p<180;p++);
+  {
+    motor.write(p);
+    delay(10);
+  }
+  for(p=180;p>=1;p--);
+  {
+   motor.write(p);
+   delay(10);
+  }
+}
